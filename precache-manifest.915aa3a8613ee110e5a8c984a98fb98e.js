a4b07912e5bd536a12a47007932f1f7627387580
Kafil Nasdami <kafil.nasdami@gmail.com>
2020-02-08 17:58:15 +0100

    Updates

---
+++ precache-manifest.915aa3a8613ee110e5a8c984a98fb98e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cd9e138405e19054e669d7f257e69bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bea3f4676e738254e453",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "21fbcf2528a4fe80dd6a",
+    "url": "/static/js/2.99404dc5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.99404dc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bea3f4676e738254e453",
+    "url": "/static/js/main.53594fef.chunk.js"
+  },
+  {
+    "revision": "44c75df54e2d168927d1",
+    "url": "/static/js/runtime-main.b82c76ed.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
