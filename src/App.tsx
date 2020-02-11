a44895e2c6ad11fecdd3d14573331a7da95181ab
Kafil Nasdami <kafil.nasdami@gmail.com>
2020-02-11 20:45:52 +0100

    adding some blank components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import { Button } from 'antd'
-import './App.css';
+import React from "react";
+import { Button } from "antd";
+import "./App.css";
 
 const App = () => (
   <div className="App">
-    <Button type="primary">Button</Button>
+    <Button type="primary" icon="book">
+      Make CV Better
+    </Button>
   </div>
 );
 
---
+++ src/components/layout/SignedInLinks.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SignedInLinks() {
+  return <div></div>;
+}
+
+export default SignedInLinks;
